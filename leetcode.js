6752bba1455270970ea31a5bed121180f40282bd
Sujan Prashanth P S <152985592+Sujanprashanth@users.noreply.github.com>
2025-07-30 18:51:12 +0530

    Update leetcode.js

--- leetcode.js
+++ leetcode.js
@@ -160,3 +160,43 @@ console.log(result);
 OUTPUT:
 1
 // -----------------------------------------------------------------------------------------------------------------------------------------------------------------
+326. POWER OF THREE
+Given an integer n, return true if it is a power of three. Otherwise, return false.
+An integer n is a power of three, if there exists an integer x such that n == 3x.
+
+Example 1:
+Input: n = 27
+Output: true
+Explanation: 27 = 33
+
+Example 2:
+Input: n = 0
+Output: false
+Explanation: There is no x where 3x = 0.
+
+Example 3:
+Input: n = -1
+Output: false
+Explanation: There is no x where 3x = (-1).
+
+PROGRAM IN JS
+var isPowerOfThree = function(n) {
+    if(n<=0){
+        return false;
+    }
+    while(n>1){
+        n=n/3;
+    }
+    if(n===1){
+        return true;
+    }
+    else{
+        return false;
+    }
+};
+const a = isPowerOfThree(81);
+console.log(a);
+
+OUTPUT
+True
+// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------
